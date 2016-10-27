26a9cf7faecd01b0db97b517cd80a050aa7e0972
Alberto Grespan <alberto@albertogrespan.com>
2016-10-27 19:41:17 -0400

    Add call_procedure_with_timeout

--- lib/voltdb/client.rb
+++ lib/voltdb/client.rb
@@ -22,18 +22,25 @@ module Voltdb
       @java_client = java_client
     end
 
-    def call_procedure(proc_name, *params)
-      @java_client.call_procedure(proc_name, *params_to_java_objects(*params))
-    end
-
     def java_client
       @java_client
     end
 
+    def call_procedure(proc_name, *params)
+      @java_client.call_procedure(proc_name, *params_to_java_objects(*params))
+    end
+
+    def call_procedure_with_timeout(query_timeout, proc_name, *params)
+      @java_client.call_procedure(query_timeout, proc_name, *params_to_java_objects(*params))
+    end
+
+    def get_instance_id
+      @java_client.get_instance_id.to_ary
+    end
+
     def_delegators :@java_client,
-      :create_connection, :call_procedure_with_timeout,
-      :update_application_catalog, :update_classes, :drain, :close,
-      :create_stats_context, :get_instance_id, :get_build_string,
+      :create_connection, :update_application_catalog, :update_classes, :drain,
+      :close, :create_stats_context, :get_build_string,
       :get_throughput_and_outstanding_txn_limits, :get_connected_host_list,
       :write_summary_csv, :get_new_bulk_loader, :call_all_partition_procedure
   end
