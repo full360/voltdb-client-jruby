df733d83cc2650cd76ea85b39817b0491565a080
Alberto Grespan <alberto@albertogrespan.com>
2016-10-28 18:10:16 -0400

    Autoload the BulkLoaderFailureCallback

--- lib/voltdb.rb
+++ lib/voltdb.rb
@@ -3,10 +3,11 @@ require "forwardable"
 require "date"
 
 module Voltdb
-  autoload :ClientConfig,             "voltdb/client"
-  autoload :ClientUtils,              "voltdb/client_utils"
-  autoload :Client,                   "voltdb/client"
-  autoload :ProcCallback,             "voltdb/callbacks"
-  autoload :AllPartitionProcCallback, "voltdb/callbacks"
-  autoload :VERSION,                  "voltdb/version"
+  autoload :ClientConfig,              "voltdb/client"
+  autoload :ClientUtils,               "voltdb/client_utils"
+  autoload :Client,                    "voltdb/client"
+  autoload :ProcCallback,              "voltdb/callbacks"
+  autoload :BulkLoaderFailureCallback, "voltdb/callbacks"
+  autoload :AllPartitionProcCallback,  "voltdb/callbacks"
+  autoload :VERSION,                   "voltdb/version"
 end
